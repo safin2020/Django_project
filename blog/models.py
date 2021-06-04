043c5fb7a435d1bcd97a0add87341405e984cf52
unknown <alsafin67@gmail.com>
2021-06-05 00:49:20 +0600

    newone

--- blog/models.py
+++ blog/models.py
@@ -62,21 +62,3 @@ class Comment(models.Model):
         return self.name
     
 
-'''
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,
-                            unique=True)
-
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("blog:blog_by_category", args={self.slug})
-
-'''
